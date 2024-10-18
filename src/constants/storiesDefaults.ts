8d1b7f787394b17e5055d1a77c52bebb1b303813
Julia K <korchkova.yulia@gmail.com>
2024-10-18 08:21:52 +0300

    feat: add stories defaults for consistency

---
+++ src/constants/storiesDefaults.ts
@@ -0,0 +1,17 @@
+import { StaticImageData } from 'next/image';
+
+export const STORY_DEFAULT_IMAGE_URL =
+  'https://www.seahouseimagery.com/cdn/shop/products/WM_Blue_Water_1080x.jpg?v=1560578927';
+
+export const STORY_DEFAULT_IMAGE_OBJ = {
+  height: 77,
+  src: STORY_DEFAULT_IMAGE_URL,
+  width: 97,
+} as StaticImageData;
+
+export const DEFAULT_LINKS = {
+  facebook: 'https://facebook.com/johndoe',
+  instagram: 'https://instagram.com/johndoe',
+  linkedin: 'https://linkedin.com/in/johndoe',
+  twitter: 'https://twitter.com/johndoe',
+};
